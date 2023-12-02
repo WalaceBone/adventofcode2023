35210ce7f2c87d58c960701ef3c9ae7c852b1127
Bones <heaugo@gmail.com>
2023-12-01 21:14:57 -0500

    Fix digit extraction logic in main.go

--- Day01/Chall1/main.go
+++ Day01/Chall1/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	// Get the file path from command-line argument
+
 	filePath := os.Args[1]
 
 	// Open the file
@@ -25,10 +26,15 @@ func main() {
 	result := 0
 	for scanner.Scan() {
 		line := scanner.Text()
+		log.Println("Line : ", line)
 		res := findFdigit(line)
-		res += findLdigit(line)
+		log.Println("Digit 1: ", res)
+		res1 := findLdigit(line)
+		log.Println("Digit 2:", res1)
 		tmpresult, err := strconv.Atoi(res)
-		result += tmpresult
+		tmpresult1, err := strconv.Atoi(res1)
+
+		result += tmpresult + tmpresult1
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -37,18 +43,54 @@ func main() {
 }
 
 func findLdigit(line string) string {
+	number := map[string]string{
+		"zero":  "0",
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
 	for i := len(line) - 1; i >= 0; i-- {
 		if unicode.IsDigit(rune(line[i])) {
 			return string(line[i])
+		}
+		for name, digit := range number {
+			if i-len(name)-1 <= len(line)-1 && i-len(name) >= 0 {
+				if line[i-len(name):len(name)] == name {
+					return digit
+				}
+			}
 		}
 	}
 	return ""
 }
 
 func findFdigit(line string) string {
-	for _, char := range line {
+	number := map[string]string{
+		"zero":  "0",
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	for i, char := range line {
 		if unicode.IsDigit(char) {
 			return string(char)
+		}
+		for name, digit := range number {
+			if line[i:i+len(name)] == name {
+				return digit
+			}
 		}
 	}
 	return ""
